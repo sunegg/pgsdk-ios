f19ce4f359021b88c4ce066fa99d38754a7db6e8
Sunegg <sunegggs@gmail.com>
2023-06-14 16:52:04 +0800

    pgsdk

--- Sources/pgsdk/Views/PgooView.swift
+++ Sources/pgsdk/Views/PgooView.swift
@@ -1,9 +1,9 @@
 import SwiftUI
 
-struct PgooView: View {
+public struct PgooView: View {
     @ObservedObject var state = Pgoo.shared.state
 
-    var body: some View {
+    public var body: some View {
         if state.isShowingRealVerifyView {
             RealVerifyView()
         } else if state.isShowingLoginView {
